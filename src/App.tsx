0e702a4f3d9c9e2d05e1698f4b710e2d81facaf8
jaxin007 <swatdimonbil@gmail.com>
2024-04-18 20:37:48 +0300

    Task-01: Move App Module css into App.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { AboutMeComponent } from '@/components/about-me/About-me.component.tsx';
 
 import { HeaderComponent } from './components/header/Header.component.tsx';
 
-import './App.css';
+import './App.module.css';
 
 function App() {
     return (
