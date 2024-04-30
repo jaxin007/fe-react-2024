335a3dc9c51d23de2f503cd4c72fa781f5f706ea
jaxin007 <swatdimonbil@gmail.com>
2024-04-30 20:30:48 +0300

    Task-02: add product and category model

---
+++ src/models/product.model.ts
@@ -0,0 +1,12 @@
+import type { Category } from '@/models/category.model.ts';
+
+export interface Product {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    images: string[];
+    creationAt: string;
+    updatedAt: string;
+    category: Category;
+}
