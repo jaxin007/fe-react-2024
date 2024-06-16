9adf9eafb49ad08c0521cec267c33a9b78f1261e
jaxin007 <swatdimonbil@gmail.com>
2024-06-16 15:01:04 +0300

    Task-06: implement routing logic

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-import { Divider } from '@/assets/icons/Divider.tsx';
-import { Moon } from '@/assets/icons/Moon.tsx';
 import { SignUp } from '@/assets/icons/SignUp.tsx';
-import { Sun } from '@/assets/icons/Sun.tsx';
 import { CartIcon } from '@/components/CartIcon/CartIcon.component.tsx';
 import IconWithRedirect from '@/components/IconWithRedirect/IconWithRedirect.component.tsx';
+import { ThemesSelector } from '@/components/ThemesSelector/ThemesSelector.component.tsx';
 import { images } from '@/constants/images.constant.ts';
 import { links } from '@/constants/links.constant.ts';
 import type { Pages } from '@/types/pages.type.ts';
@@ -14,48 +13,31 @@ import type { Themes } from '@/types/themes.type.ts';
 import styles from './Header.module.css';
 
 interface HeaderProps {
-    activePage: Pages;
     totalProductsInCart: number;
     handleActivePage: (page: Pages) => void;
     theme: Themes;
     setTheme: (theme: Themes) => void;
 }
 
-export const Header: React.FC<HeaderProps> = ({ totalProductsInCart, handleActivePage, activePage, theme, setTheme }) => (
+export const Header: React.FC<HeaderProps> = ({ totalProductsInCart, handleActivePage, theme, setTheme }) => (
     <div className={styles.headerWrapper}>
         <div className={styles.header}>
             <div className={styles.leftIcons}>
                 <IconWithRedirect icon={images.maCrownIcon} url={links.home} alt={'masters academy icon'}></IconWithRedirect>
-                <div className={styles.themes}>
-                    <button className={styles.moonIcon} onClick={() => setTheme('dark')}>
-                        <Moon theme={theme} />
-                    </button>
-                    <div className={styles.verticalDivider}>
-                        <Divider theme={theme} />
-                    </div>
-                    <button className={styles.sunIcon} onClick={() => setTheme('light')}>
-                        <Sun theme={theme} />
-                    </button>
-                </div>
+                <ThemesSelector setTheme={setTheme} />
             </div>
             <div className={styles.rightIcons}>
                 <div className={styles.pageTabs}>
-                    <button
-                        className={activePage === 'about' ? styles.activeButton : styles.inactiveButton}
-                        onClick={() => handleActivePage('about')}
-                    >
+                    <NavLink to={'/'} className={({ isActive }) => (isActive ? styles.activeButton : styles.inactiveButton)}>
                         About
-                    </button>
-                    <button
-                        className={activePage === 'products' ? styles.activeButton : styles.inactiveButton}
-                        onClick={() => handleActivePage('products')}
-                    >
+                    </NavLink>
+                    <NavLink to={'/products'} className={({ isActive }) => (isActive ? styles.activeButton : styles.inactiveButton)}>
                         Products
-                    </button>
+                    </NavLink>
                 </div>
 
                 <CartIcon counter={totalProductsInCart} handleCartIcon={() => handleActivePage('products')}></CartIcon>
-                <IconWithRedirect className={styles.menuDuoIcon} icon={images.menuDuoIcon} url="/#"></IconWithRedirect>
+                <img className={styles.menuDuoIcon} src={images.menuDuoIcon} alt={'menu'} />
                 <button className={styles.signInButton}>
                     <img src={images.signInIcon} alt="sign in button" />
                     <p>Login</p>
